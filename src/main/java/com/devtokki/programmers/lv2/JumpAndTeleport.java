1d898fce185976c74ccf977086480c2b6c8ecc43
jhpark <kevin.jhpark@sparrwofasoo.com>
2022-09-15 10:01:51 +0900

    MODIFY: JumpAndTeleport

--- src/main/java/com/devtokki/programmers/lv2/JumpAndTeleport.java
+++ src/main/java/com/devtokki/programmers/lv2/JumpAndTeleport.java
@@ -1,2 +1,17 @@
-package com.devtokki.programmers.lv2;public class JumpAndTeleport {
+package com.devtokki.programmers.lv2;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12980
+ */
+public class JumpAndTeleport {
+    public int solution(int n) {
+        int ans = 0;
+        while(n > 1) {
+            if( n % 2 != 0)
+                ++ans;
+
+            n = n / 2;
+        }
+        return ans + 1;
+    }
 }
--- src/test/java/com/devtokki/programmers/lv2/JumpAndTeleportTest.java
+++ src/test/java/com/devtokki/programmers/lv2/JumpAndTeleportTest.java
@@ -1,4 +1,48 @@
+package com.devtokki.programmers.lv2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class JumpAndTeleportTest {
-  
+
+    @Test
+    void solutionTest1() {
+//        Given
+        Integer oracle = 2;
+        Integer inputValue = 5;
+        JumpAndTeleport quiz = new JumpAndTeleport();
+
+//        When
+        Integer ret = quiz.solution(inputValue);
+//        Then
+        Assertions.assertEquals(oracle, ret);
+    }
+
+    @Test
+    void solutionTest2() {
+//        Given
+        Integer oracle = 2;
+        Integer inputValue = 6;
+        JumpAndTeleport quiz = new JumpAndTeleport();
+
+//        When
+        Integer ret = quiz.solution(inputValue);
+//        Then
+        Assertions.assertEquals(oracle, ret);
+    }
+
+    @Test
+    void solutionTest3() {
+//        Given
+        Integer oracle = 5;
+        Integer inputValue = 5000;
+        JumpAndTeleport quiz = new JumpAndTeleport();
+
+//        When
+        Integer ret = quiz.solution(inputValue);
+//        Then
+        Assertions.assertEquals(oracle, ret);
+    }
 }
